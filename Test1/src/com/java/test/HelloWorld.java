271cce7ae15acbb53da84820bb3ccc3710fb780b
liam <liam@easestrategy.com>
2021-01-20 09:53:09 +0800

    feats:test2

--- Test1/src/com/java/test/HelloWorld.java
+++ Test1/src/com/java/test/HelloWorld.java
@@ -8,5 +8,6 @@ public class HelloWorld {
     public static void main(String[] args){
         System.out.println("HelloWorld!");
         System.out.println("2");
+        System.out.println("develop");
     }
 }
