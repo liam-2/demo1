cd665ffdf927da9330c4ad9f625732152e5a8d39
liam <liam@easestrategy.com>
2021-01-20 09:48:24 +0800

    feats:eatablish a test

---
+++ Test1/src/com/java/test/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.java.test;
+
+/**
+ * @Author: liam
+ * @Date: 2021/1/20 9:40
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("HelloWorld!");
+    }
+}
